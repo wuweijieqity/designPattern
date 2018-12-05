a80e310081844a2247b09a079cbea20bf33f57e8
wuweijieqity <31516968+wuweijieqity@users.noreply.github.com>
2018-12-05 17:40:38 +0800

    增加抽象工厂模式

--- src/creational_pattern/Test.java
+++ src/creational_pattern/Test.java
@@ -1,14 +1,30 @@
 package creational_pattern;
 
-import com.ctrip.finance.creditcard.autotest.designPattern.creational_pattern.simplefactorypattern.Apple;
-import com.ctrip.finance.creditcard.autotest.designPattern.creational_pattern.simplefactorypattern.Fruit;
-import com.ctrip.finance.creditcard.autotest.designPattern.creational_pattern.simplefactorypattern.FruitFactory;
+import creational_pattern.AbstractFactoryPattern.abstractMilkTea.BubbleMilkTea;
+import creational_pattern.AbstractFactoryPattern.abstractMilkTea.JellyMilkTea;
+import creational_pattern.AbstractFactoryPattern.abstractMilkTea.TaroMilkTea;
+import creational_pattern.AbstractFactoryPattern.abstractmilkteafactory.MilkTeaFactory;
+import creational_pattern.AbstractFactoryPattern.concretefactory.CoCo;
+import creational_pattern.simplefactorypattern.Apple;
+import creational_pattern.simplefactorypattern.Fruit;
+import creational_pattern.simplefactorypattern.FruitFactory;
 
 public class Test {
+
+
     public static void main(String args[]) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        /*水果测试类，简单工厂方法
         Fruit fruit;
         fruit = (Apple)FruitFactory.getFruitProduct("Apple");
         fruit.fruitColor();
-
+        */
+        //抽象工厂方法
+        BubbleMilkTea cocobubbleMilkTea;
+        JellyMilkTea jellyMilkTea;
+        TaroMilkTea taroMilkTea;
+        MilkTeaFactory milkTeaFactory;
+        milkTeaFactory = new CoCo();
+        cocobubbleMilkTea = milkTeaFactory.makeBubbleMilkTea();
+        cocobubbleMilkTea.make();
     }
 }
